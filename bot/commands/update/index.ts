9de7f4eaa5a8f0973126b68f1b28d67323f335cd
Pierre Yager <pierre.y@gmail.com>
2024-11-19 00:18:24 +0100

    Improvements : * parse args using utils.parseArgs() * use a command syntax (bun run main.ts <command> [--dry-run] * added --dry-run option to new and update commands * added support for % float values in .env BUY_OFFSET and SELL_OFFSET (eg. 2, 2000 or 2.5%) * a little refactoring for readability

--- bot/commands/update/index.ts
+++ bot/commands/update/index.ts
@@ -2,66 +2,80 @@ import * as Exchange from '../../services/exchange';
 import * as Cycle from '../../database/cycle';
 import {type CycleType, type OrderType, Status} from '../../types';
 
-export default async function () {
+export default async function (dryRun: boolean) {
+    // Get last BTC price in USDT
     const lastPrice = (await Exchange.getLastPrice()).lastPriceNumber as number;
 
+    // Read running cycles from database
     const uncompletedCycles = Cycle.listUncompleted() as Array<CycleType>
     for (const cycle of uncompletedCycles) {
 
         const {id: cycleId, status, order_buy_id, order_sell_price, quantity, order_sell_id}: CycleType = cycle;
 
-        if (status === Status.ORDER_BUY_PLACED) {
-            const order = await Exchange.getOrder(order_buy_id as string)
+        switch(status) {
+            case Status.ORDER_BUY_PLACED:
+                const buy_order = await Exchange.getOrder(order_buy_id as string)
 
-            if (order.isActive) {
-                console.log(`Buy order ${order_buy_id} still active`);
-
-            } else {
-                console.log(`Buy order ${order_buy_id} filled`);
-
-                console.log(`Start updating cycle ${cycle.id}`);
-
-                Cycle.updateStatus(cycleId as number, Status.ORDER_BUY_FILLED);
-
-                if ( lastPrice > Number(order_sell_price) ) {
-                    const newSellPrice = Number(cycle.order_sell_price) + 100
-                    Cycle.updateOrderSellPrice(Number(cycle.id), newSellPrice)
+                if (buy_order.isActive) {
+                    console.log(`Buy order ${order_buy_id} still active`);
+                    continue;
                 }
 
-                let price = Cycle.getById(
-                    Number(cycleId)
-                ) as string;
-                price = String(order_sell_price);
+                console.log(`Buy order ${order_buy_id} filled`);
+                console.log(`Start updating cycle ${cycle.id}`);
 
-                const orderSell: OrderType = await Exchange.createOrder({
-                    symbol: 'BTC_USDT',
-                    side: 'sell',
-                    price,
-                    quantity: String(quantity)
-                });
+                if (!dryRun) {
+                    Cycle.updateStatus(cycleId as number, Status.ORDER_BUY_FILLED);
+                }
 
-                Cycle.updateOrderSellId(
-                    Number(cycleId),
-                    String(orderSell.id)
-                );
+                if (lastPrice > Number(order_sell_price)) {
+                    // TODO: this should be a configurable constant ------v 
+                    const newSellPrice = Number(cycle.order_sell_price) + 100
+                    if (!dryRun) {
+                        Cycle.updateOrderSellPrice(Number(cycle.id), newSellPrice)
+                    }
+                }
 
-                Cycle.updateStatus(
-                    Number(cycleId),
-                    Status.ORDER_SELL_PLACED
-                );
-            }
-        } else if (status === Status.ORDER_SELL_PLACED) {
-            const order = await Exchange.getOrder(order_sell_id as string)
-            if (order.isActive) {
-                console.log(`Sell order ${order_sell_id} still active`);
-            } else {
+                if (!dryRun) {
+                    let price = Cycle.getById(
+                        Number(cycleId)
+                    ) as string;
+                    price = String(order_sell_price);
+
+                    const orderSell: OrderType = await Exchange.createOrder({
+                        symbol: 'BTC_USDT',
+                        side: 'sell',
+                        price,
+                        quantity: String(quantity)
+                    });
+
+                    Cycle.updateOrderSellId(
+                        Number(cycleId),
+                        String(orderSell.id)
+                    );
+
+                    Cycle.updateStatus(
+                        Number(cycleId),
+                        Status.ORDER_SELL_PLACED
+                    );
+                }
+                break;
+
+            case Status.ORDER_SELL_PLACED:
+                const sell_order = await Exchange.getOrder(order_sell_id as string)
+                if (sell_order.isActive) {
+                    console.log(`Sell order ${order_sell_id} still active`);
+                    continue;
+                }
+            
+                // Even in --dry-run mode, this should be updated in database
                 Cycle.updateStatus(
                     Number(cycleId),
                     Status.COMPLETED
                 );
 
                 console.log(`Cycle ${cycleId} successfully completed`);
-            }
+                break;
         }
     }
 
--- main.ts
+++ main.ts
@@ -1,36 +1,58 @@
+import { parseArgs } from "util";
+
 import check from './bot/commands/check';
 import server from './bot/commands/server'
 import startNewCycle from './bot/commands/new';
-import update from './bot/commands/update';
+import updateCycles from './bot/commands/update';
 
 function menu() {
-    console.log('\nSimple Trading Bot v2 \n');
-    console.log('--check      -c    Check config \n');
-    console.log('--new        -n    Start new cycle \n');
-    console.log('--update     -u    Update running cycles \n');
-    console.log('--server     -s    Run server \n');
+    console.log('\nSimple Trading Bot v2.1');
+    console.log('\nCommands: (c)heck, (n)ew, (u)pdate, (s)erve\n')
+    console.log('   check                      Check config');
+    console.log('   new        [--dry-run]     Start new cycle');
+    console.log('   update     [--dry-run]     Update running cycles');
+    console.log('   serve                      Run server');
+    console.log('\nParameters:')
+    console.log('   --dry-run                  Do not place order in Exchange (and do not change the database)')
     process.exit();
 }
 
-const lastArg = Bun.argv.at(-1);
+const { values, positionals } = parseArgs({
+    args: Bun.argv,
+    options: {
+        "dry-run": {
+            type: 'boolean',
+            default: false
+        }
+    },
+    strict: true,
+    allowPositionals: true,
+});
 
-switch (lastArg) {
-    case '-c':
-    case '--check':
+const command = positionals.at(-1)
+const dryRun = values["dry-run"] || false
+
+switch (command) {
+    case 'c':
+    case 'check':
         await check();
         break;
-    case '-n':
-    case '--new':
-        await startNewCycle();
+    
+    case 'n':
+    case 'new':
+        await startNewCycle(dryRun);
         break;
-    case '-u':
-    case '--update':
-        await update();
+    
+    case 'u':
+    case 'update':
+        await updateCycles(dryRun);
         break;
-    case '-s':
-    case '--server':
+        
+    case 's':
+    case 'serve':
         server();
         break;
+
     default:
         menu();
 }
